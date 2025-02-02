55f1fbb79fd463df88041b570ac9f83f49e645f6
Hiller27 <hillersilva7@gmail.com>
2025-02-02 15:03:27 -0500

    update_version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },  // Ruta para la página de inicio
+    { path: 'about', component: HomeComponent },  // Ruta para la página de "Acerca de"
+  ]
